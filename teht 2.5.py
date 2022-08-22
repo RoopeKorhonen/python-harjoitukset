9e1a34a308de5bb89d0b63685cf204f0e185c04f
Roope <roope.korhonen2@metropolia.fi>
2022-08-22 16:20:08 +0300

    5 commit

---
+++ teht 2.5.py
@@ -0,0 +1,29 @@
+import math
+#tiedot
+L = float(input("anna luotien määrä "))
+N = float(input("anna naulojen määrä"))
+LE = float(input("anna leivisköjen määrä"))
+luoti = 13.3
+naula = 32 * luoti
+leiviskä = 20 * naula
+
+#laskutoimet
+
+g = (luoti * L + naula * N + leiviskä * LE)
+kg = g // 1000
+g2 = g % 1000
+
+
+#tulostus
+
+print(f"leiviskät {LE}")
+print(f"naulat {N}")
+print(f"luodit {L}")
+
+print(f"Massa nykymittojen mukaan: {kg} kilogrammaa ja {g2:6.1f} grammaa")
+
+
+
+
+
+
---
+++ teht 2.6.py
@@ -0,0 +1,6 @@
+import random
+
+x = random.randint(0, 9)
+print(x)
+
+y = random.randint(1, 6)
