9cd088910e53dc49b65d694b36863a9440526cb2
Roope <roope.korhonen2@metropolia.fi>
2022-08-22 14:52:57 +0300

    4 commit

---
+++ teht 2.4.py
@@ -0,0 +1,16 @@
+Luku_1 = int(input("Anna kokonaisluku "))
+
+Luku_2 = int(input("Anna kokonaisluku "))
+
+Luku_3 = int(input("Anna kokonaisluku "))
+
+#laskutoimet
+
+summa = Luku_1 + Luku_2 + Luku_3
+tulo = Luku_1 * Luku_2 * Luku_3
+ka = summa / 3
+
+#tulostus
+print(summa)
+print(tulo)
+print(ka)
